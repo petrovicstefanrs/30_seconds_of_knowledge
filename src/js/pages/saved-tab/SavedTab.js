c18aa6496b003ab7957914bea2bda1cb992360af
Ahmad Santarissy <ahmad.s@cgiandi.com>
2019-02-11 00:33:07 +0200

    saved tab styles and delete logic

--- src/js/pages/saved-tab/SavedTab.js
+++ src/js/pages/saved-tab/SavedTab.js
@@ -1,19 +1,102 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader';
 
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import Spinner from '../../components/spinner';
+import Chip from '../../components/chip';
+import trashIconSrc from '../../../assets/images/icons/trash.svg';
+import {restoreSnippetsFromStorage, saveSnippetToStorage} from '../../api/storage';
 
 import './savedTab.css';
 
 const CLASS = 'sok-SavedTab';
 
 class SavedTab extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			snippets: null,
+		};
+	}
+
+	componentDidMount() {
+		this.initSnippetsFromStorage();
+	}
+
+	initSnippetsFromStorage = async () => {
+		let snippets = await restoreSnippetsFromStorage();
+
+		this.setState({
+			snippets,
+		});
+	};
+
+	renderSpinner = () => {
+		const {snippets} = this.state;
+
+		if (snippets) {
+			return null;
+		}
+
+		return <Spinner />;
+	};
+
+	renderLangChip = lang => {
+		if (!lang) {
+			return null;
+		}
+
+		return <Chip value={lang} />;
+	};
+
+	handleSnippetClick = () => {};
+
+	handleSnippetDelete = async snippet => {
+		const {snippets} = this.state;
+		_.remove(snippets, item => item.title === snippet.title);
+
+		await saveSnippetToStorage(snippets);
+
+		this.setState({snippets});
+	};
+
+	renderSnippets = () => {
+		const {snippets} = this.state;
+
+		if (!snippets) {
+			return this.renderSpinner();
+		}
+
+		const snippetsItems = snippets.map((snippet, index) => {
+			return (
+				<div key={index} className={`${CLASS}-item`}>
+					<div className={`${CLASS}-title`} onClick={() => this.handleSnippetClick(snippet)}>
+						{snippet.title}
+					</div>
+					{this.renderLangChip(snippet.language)}
+					<div className={`${CLASS}-delete`} onClick={() => this.handleSnippetDelete(snippet)}>
+						<img src={trashIconSrc} alt="trash button" />
+					</div>
+				</div>
+			);
+		});
+
+		return (
+			<React.Fragment>
+				<h2>Saved Snippets</h2>
+				{snippetsItems}
+			</React.Fragment>
+		);
+	};
+
 	render() {
 		return (
 			<div className={CLASS}>
 				<Header renderOptionsBtn={false} />
-				<span className={`${CLASS}-contentContainer`}> </span>
+				<div className={`${CLASS}-contentContainer`}>{this.renderSnippets()}</div>
 				<Footer />
 			</div>
 		);
